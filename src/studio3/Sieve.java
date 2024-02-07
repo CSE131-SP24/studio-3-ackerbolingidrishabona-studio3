98c3c77e5b004d8442a7e6a64b87b7654c2e2e71
sophiaacker <sophiaacker@localhost>
2024-02-07 14:22:41 -0600

    sieve

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,33 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+	
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("What is n?");
+		int valueN = in.nextInt();
+		
+		int[] numbers = new int[valueN + 1];
+		
+		for(int i = 0; i < numbers.length; i++)
+		{ ; 
+			numbers[i] = i; 
+		}
+		
+		boolean[] newNumbers = new boolean[valueN + 1];
+		
+		for (i = 0: i < sqrt(valueN) + 1)
+		{
+		
+		}
+		
+	}
+
+}
+
+
+
